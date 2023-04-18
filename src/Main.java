e2fa84e03c6f35676c4afc163d7366e17a2586a0
GitChrisCode <chrha.gw2@gmail.com>
2023-04-18 11:49:46 +0200

    New Important Message added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("     This is Git ...                      ");
+
+        System.out.println("Throw out ...");
+        System.out.println("Throw in ...");
     }
 }
